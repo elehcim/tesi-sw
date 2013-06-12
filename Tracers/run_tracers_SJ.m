61f0a89d2b45137237790f1a4e2b11b421b8ea3f
Michele Mastropietro <michele.mastropietro@gmail.com>
2013-06-12 23:43:34 +0200

    clearvars better than clear all in this case

--- Tracers/run_tracers_SJ.m
+++ Tracers/run_tracers_SJ.m
@@ -1,10 +1,9 @@
-clear all
+clearvars
 tr=select_tracers()
 % tr=select_tracers('Sun_Jupiter_t=220_little.fig');
-% load OcchioGawlik_tracers_11:20:9
-% load Sun_Jupiter_t=220_little_tracers_183348 % Tracers con x = x_earth
+% load Sun_Jupiter_t=220_little_tracers_183348  % Tracers con x = x_earth
 % load Sun_Jupiter_v_t=220_tracers_171341		% Tracers con impatto
-%generate_tracers_SJ
+% generate_tracers_SJ
 traj=integrate_tracers(tr);
 figure
 plot_traj(tr,traj)
