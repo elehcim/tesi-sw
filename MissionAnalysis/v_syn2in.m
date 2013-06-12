96fd69d6e183ad66e6896cccb45c117baa6a9739
Michele Mastropietro <michele.mastropietro@gmail.com>
2013-06-12 13:05:51 +0200

    improved formatting

--- MissionAnalysis/v_syn2in.m
+++ MissionAnalysis/v_syn2in.m
@@ -1,11 +1,11 @@
 function [X_dot,Y_dot]=v_syn2in(x,y,vx,vy,nu, a,e, grav_par)
 % Transform the synodic velocity to the inertial velocity
-%pag. 5  Gurfil and Meltzer
+% pag. 5  Gurfil and Meltzer
 % "Stationkeeping: Generalization to the Elliptic Restricted Three-Body Problem"
 p = a*(1-e^2);
-r = p/(1+e*cos(nu));
 %{
 % First version
+r = p/(1+e*cos(nu));
 r_prime = p*e*sin(nu)/(1+e*cos(nu))^2;
 nu_dot = sqrt(grav_par) / (p^(3/2))*(1+e*cos(nu))^2;
 X_dot = nu_dot* ( r_prime*x + r*vx );
