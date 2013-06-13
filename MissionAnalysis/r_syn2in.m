43d9cbc04ab4d04b9a0d23486258f2f4e6bb3be1
Michele Mastropietro <michele.mastropietro@gmail.com>
2013-06-13 23:30:57 +0200

    little fixes

--- MissionAnalysis/r_syn2in.m
+++ MissionAnalysis/r_syn2in.m
@@ -1,11 +1,11 @@
 function [X,Y]=r_syn2in(x,y,nu, a,e)
-% Transform the synodic position coordinate to the inertial positions
-% a,e are the semimajor axis and the eccentricity of the orbit of one
+% Transform the synodic position coordinate to the inertial positions.
+% a,e are the semimajor axis and the eccentricity of the orbit of one 
 % primary with respect to the other
 %% Distance between primaries
 r = a*(1-e^2)/(1+e*cos(nu));
 
-%% Bring to not rotating frame (Kolomaro pag. 27, formula 2.3.1)
+%% Get to not rotating frame (Kolomaro pag. 27, formula 2.3.1)
 x_prime = x * cos(nu) - y * sin(nu);
 y_prime = x * sin(nu) + y * cos(nu);
 
