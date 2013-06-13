e94c174884a497b6b2f1424034154cde9f8c4243
Michele Mastropietro <michele.mastropietro@gmail.com>
2013-06-14 00:26:49 +0200

    implemented

--- MissionAnalysis/tracers_grid_SJ_little.m
+++ MissionAnalysis/tracers_grid_SJ_little.m
@@ -1,4 +1,5 @@
 function tr=tracers_grid_SJ_little
+n=3;
 %% Get the param_struc structure in the figure UserData field.
 fig_file='../Tracers/Sun_Jupiter_t=220_little.fig';
 fig=openfig(fig_file);
@@ -14,17 +15,20 @@ vx_min=h.YLim(1);
 vx_max=h.YLim(2);
 
 %% Setup the tr structure
-n=5;
-tr.n_tracers=n;
-tr.x=linspace(x_min,x_max,tr.n_tracers);
+tr.n_tracers=n^2;
+
+x_val=linspace(x_min,x_max,n);
+vx_val=linspace(vx_min,vx_max,n);
+[X,Y]=meshgrid(x_val,vx_val);
+tr.x=X(:)';
+tr.vx=Y(:)';
+
 %tr.x=-0.1922*ones(1,n);
-tr.y=param_struc.y_0*ones(1,n);
-tr.vx=linspace(vx_min,vx_max,tr.n_tracers);
 %tr.vx=linspace(1.534,1.789,n); % these value are proper of the LCS
-tr.e=param_struc.e_0*ones(1,n);
+tr.y=param_struc.y_0*ones(1,n^2);
+tr.e=param_struc.e_0*ones(1,n^2);
 tr.t0=param_struc.t0;
 tr.T=param_struc.DT;
 tr.mu=param_struc.mu;
 tr.ecc=0.04839; % FIXME Attenzione
 tr=complete_tracers(tr); % compute vy
-disp(tr)
