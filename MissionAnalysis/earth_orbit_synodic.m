c761d2c367509018cc69cfca1be0d00d2d046b1b
Michele Mastropietro <michele.mastropietro@gmail.com>
2013-06-20 17:01:53 +0200

    renamed and added plot

---
+++ MissionAnalysis/earth_orbit_synodic.m
@@ -0,0 +1,17 @@
+clearvars
+tempi=linspace(0,(2*pi)/11,12);
+
+sun_grav_par = 132712439935; % km^3/s^2
+a_jup = 778412027; % km
+ecc_jup = 0.04839266;
+R = 149600000;	% km
+omega= 29.7830/R;	% rad/s
+
+for j=1:length(tempi)
+[x(j),y(j)]=earth_synodic(tempi(j), sun_grav_par, a_jup, ecc_jup, omega, R);
+end
+figure
+hold all
+plot(x,y)
+plot(x,y,'o')
+hold off
