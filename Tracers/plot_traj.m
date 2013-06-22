3a80240c74cde78e841ac72c7a612ffda53683a7
Michele Mastropietro <michele.mastropietro@gmail.com>
2013-06-22 18:50:04 +0200

    little fix

--- Tracers/plot_traj.m
+++ Tracers/plot_traj.m
@@ -2,15 +2,14 @@ function plot_traj(mu,traj,index)
 if nargin < 3
 	index = 0;
 end
-a=size(traj);
-n=a(1);
+n=size(traj,1);
 for i=1:n
 	hold all
 	plot(traj{i,1}(:,1),traj{i,1}(:,2))
 	leg{i}=sprintf(' %d',i);
 end
 if nargin == 3
-	for j=1:length(index) % Useful to plot the chosen value when optimizing
+	for j=1:length(index) % Useful to label the chosen value when optimizing
 	leg{j}=sprintf(' %d',index(j));
 	legend(leg,'fontsize',10)
 	end
