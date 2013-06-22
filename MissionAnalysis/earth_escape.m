b68002d7a2cc9a763fdb87145cf93230667cae4b
Michele Mastropietro <michele.mastropietro@gmail.com>
2013-06-22 17:32:15 +0200

    Implemented

--- MissionAnalysis/earth_escape.m
+++ MissionAnalysis/earth_escape.m
@@ -1,11 +1,21 @@
-function dv=earth_escape()
+function dv=earth_escape(x,y,vx,vy)
+% assuming circular earth orbit around the sun
+
 R_earth=6378; % km
-GM_earth=3.986e5 % km^3/s^2
+GM_earth=3.986e5; % km^3/s^2
+
+v_planet=29.783; % km/s
 
 % LEO orbit altitude
 h = 300; % km
 
-v_excess=
+% Earth orbital velocity
+[theta,rho]=cart2pol(x,y);
+
+vx_earth=-v_planet*sin(theta);
+vy_earth=v_planet*cos(theta);
+
+v_excess=sqrt((vx-vx_earth)^2+(vy-vy_earth)^2);
 
 rp=R_earth+h;
 
