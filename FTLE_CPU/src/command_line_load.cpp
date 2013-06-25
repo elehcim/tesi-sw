be87eab9fc8db8b9b1daaecc4939b5c46a35fcf4
Michele Mastropietro <michele.mastropietro@gmail.com>
2013-06-25 11:45:45 +0200

    changed parameter

--- FTLE_CPU/src/command_line_load.cpp
+++ FTLE_CPU/src/command_line_load.cpp
@@ -10,13 +10,13 @@ std::string command_line_load (int ac, char* av[])
 {
 po::options_description cmdline_options("Allowed options on command line");
     cmdline_options.add_options()
-        ("input_file", po::value<std::string>()->default_value("Configuration.cfg"), "Name of the input file")
+        ("input-file", po::value<std::string>()->default_value("Configuration.cfg"), "Name of the input file")
         ;
 
 po::variables_map vm;
 po::store(po::parse_command_line (ac,av,cmdline_options), vm);
 po::notify(vm);
 
-config_file=vm["input_file"].as<std::string>();
+config_file=vm["input-file"].as<std::string>();
 return config_file;
 }
