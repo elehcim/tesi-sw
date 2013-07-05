308fe5240a31737ceaf301ee67bcc0ac5561f5d2
Michele Mastropietro <michele.mastropietro@gmail.com>
2013-07-05 15:29:53 +0200

    fixes definitive

--- MissionAnalysis/hist_dv.m
+++ MissionAnalysis/hist_dv.m
@@ -6,7 +6,7 @@ addpath('../Tracers/')
 load data_2500_grid_dv
 %% best n dv
 % from http://stackoverflow.com/questions/2692482/get-the-indices-of-the-n-largest-elements-in-a-matrix
-n=50;
+n=29;
 dv_total_copy = dv_total;
 for j = 1:n
    [a, Index(j)] = min(dv_total_copy);
@@ -22,9 +22,10 @@ for j=1:n_tracers_in_SOI
 transfer_time_in_SOI(j)=traj_in_SOI{j,2}(end);
 end
 % correlation dv vs. transfer time
-DV=dv_total(logical(tracers_in_SOI));
-T=transfer_time_in_SOI;
-scatter(T*11.86/(2*pi)*12,DV)
+DV=dv_total(logical(tracers_in_SOI(Index)));
+T=transfer_time(Index);
+scatter(T*11.86/(2*pi)*12,DV,40,'filled')
+ylim([0 20])
 xlabel('Transfer time (months)','interpreter','latex','fontsize',35)
 ylabel('$\Delta v$ (km/s)','interpreter','latex','fontsize',35)
 
@@ -65,7 +66,7 @@ hold on;
 % x_=meshgrid(tr.vx(Index),tr.vy(Index));
 % y_=meshgrid(tr.vx(Index),tr.vy(Index));
 scatter3(tr.vx(Index),tr.vy(Index),dv_total(Index),80,...
-	dv_total(Index),'filled');
+	'r','filled'); % color_span(Index))
 zlabel('$\Delta v$','interpreter','latex','fontsize',35)
 for j=1:n
 plot3([tr.vx(Index(j)),tr.vx(Index(j))],...
@@ -77,24 +78,32 @@ grid on
 pbaspect([1,1,.2])
 hold off
 
-%% all with lines
+%% all
 fi=openfig([folder 'MissionAnalysis/Prove per missione/9luglio/6 to_dv_grid/to_dv_grid_300.fig']);
 set(fi,'name','dv scatter 3d')
 hold on;
 % x_=meshgrid(tr.vx(Index),tr.vy(Index));
 % y_=meshgrid(tr.vx(Index),tr.vy(Index));
 color_span=(dv_total-min(dv_total))/(max(dv_total)-min(dv_total))*(2.2-1.1)+1.1;
-scatter3(tr.vx,tr.vy,color_span, 80,'r','filled');
+scatter3(tr.vx,tr.vy,dv_total, 15,color_span,'filled');
+% g=gridfit(tr.vx,tr.vy,dv_total,100,100);
+% xcoord=linspace(min(tr.vx),max(tr.vx),100);
+% ycoord=linspace(min(tr.vy),max(tr.vy),100);
+% F=TriScatteredInterp(tr.vx',tr.vy',dv_total')
+% zcoord=F(xcoord,ycoord);
+% mesh(xcoord,ycoord,zcoord);
+
 zlabel('$\Delta v$','interpreter','latex','fontsize',35)
-for j=1:tr.n_tracers
-plot3([tr.vx(j),tr.vx(j)],...
-	[tr.vy(j),tr.vy(j)],...
-	[0,color_span],'k', 'linewidth',3)
-end
+% for j=1:tr.n_tracers
+% plot3([tr.vx(j),tr.vx(j)],...
+% 	[tr.vy(j),tr.vy(j)],...
+% 	[0,color_span(j)],'k', 'linewidth',3)
+% end
 % FIXME adjust z axis
 box off
+view(3)
 grid on
-pbaspect([1,1,.2])
+% pbaspect([1,1,.2])
 hold off
 %% Histogram
 figure('name','dv total')
