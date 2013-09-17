1df4afaef7f81010de5ff4a6215ca6331e93b286
Michele Mastropietro <michele.mastropietro@gmail.com>
2013-09-17 16:36:22 +0200

    fixed for actual use

--- FTLE_nu_v/Matlab/run_traj.m
+++ FTLE_nu_v/Matlab/run_traj.m
@@ -1,11 +1,8 @@
-fig_file='../ftle_nuv_mu=0.0010_ecc=0.05_n=200x200_DT=5.000000.fig';
-
+%fig_file='../ftle_nuv_mu=0.0010_ecc=0.05_n=200x200_DT=5.000000.fig';
 %%
-tr=select_tracers(fig_file);
+tr=select_tracers;
 %%
 integrated_tracers=integrate_tracers(tr);
-%%
-%contours=select_contours;
 %%
 figure
 plot_traj(tr.mu,integrated_tracers)
